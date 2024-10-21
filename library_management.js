fcc5346bd72e71ff1706d80aef2e8c0035e197a8
tcoolican <tcoolican@usf.edu>
2024-10-20 22:36:29 -0400

    Create Book class with properties and methods

--- library_management.js
+++ library_management.js
@@ -1,1 +1,23 @@
+//Coding Challenge #9: Library Management System
+//Task 1: Create a Book Class
+class Book { //Define a Class
+    constructor(title, author, ISBN) { 
+        this.title = title;
+        this.author = author; 
+        this.ISBN = ISBN;
+        this._isAvailable = true; 
+    };
+
+    getDetails() { // Book Info
+        return `Title: ${this.name}, Author: ${this.author}, ISBN: ${this.ISBN}`;
+    };
+
+    get isAvailable() { // getter to see if book is available
+        return `${this._isAvailable}`;
+    };
+
+    set isAvailable(status) { // setter to update a books availability
+        this._isAvailable = status;
+    }
+};
 
